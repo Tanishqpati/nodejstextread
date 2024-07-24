dbd782eab5481973d54f7a7a2590ce77c90ae023
Tanishq Patidar <113436940+Tanishqpati@users.noreply.github.com>
2024-07-24 15:05:05 +0530

    Create app.js
    initial commit

---
+++ app.js
@@ -0,0 +1,7 @@
+const fs = require('fs');
+const path = require('path');
+
+const filePath = path.join(__dirname, 'input.txt');
+function countWordOccurences(filePath){
+  const countWords = {};
+  cosnt 
